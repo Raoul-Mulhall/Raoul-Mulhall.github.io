61dad437414d4a0340db248373799d5c71d099f0
Raoul-Mulhall <Mulhall.ra@gmail.com>
2025-11-30 16:01:13 +0000

    Update RPScript.js

--- RPScript.js
+++ RPScript.js
@@ -1,53 +1,60 @@
-//Listens for button clicks then calls the play function and gives it the corresponding button value
+// Listens for button clicks and calls play()
 document.getElementById("rock").addEventListener("click", () => play("rock"));
 document.getElementById("paper").addEventListener("click", () => play("paper"));
 document.getElementById("scissors").addEventListener("click", () => play("scissors"));
 
-
 function getComputer() {
     const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * 3)];
-} //Makes computer return random choice 
+}
 
+// Game tracking
 let round = 0;
-let roundTotal = 3;
-
+let roundTotal = 5;
+let userGuesses = [];     //store user’s guesses
 
 function play(playerChoice) {
 
     if (round === roundTotal) {
-        document.getElementById("result").innerHTML = "Game Over!!!"
+        document.getElementById("result").innerHTML =
+            "Game Over!<br><br>Your guesses were: " + userGuesses.join(", ");
         return;
     }
 
-    const computerChoice = getComputer(); //Calls computer choice into variable
-    let result = ""; //Defines result variable as string
+    const computerChoice = getComputer();
+    let result = "";
 
-
+    // Determine the winner
     if ((playerChoice === "rock" && computerChoice === "scissors") ||
         (playerChoice === "paper" && computerChoice === "rock") ||
         (playerChoice === "scissors" && computerChoice === "paper")
     ) {
         result = "You win!!!";
-
-
     }
     else if (playerChoice === computerChoice) {
         result = "It was a tie!!!";
-
     }
     else {
         result = "You lose!!!";
-
     }
 
-    round = round + 1;
+    // Store the user’s guess
+    userGuesses.push(playerChoice);
 
+    // Advance round
+    round++;
 
-    //Creates result message and send to result div box
-    document.getElementById("result").innerHTML = "You chose " + playerChoice + ", the computer chose " + computerChoice + ", " + result;
+    // Show round result
+    document.getElementById("result").innerHTML =
+        "Round " + round + "<br>" +
+        "You chose " + playerChoice +
+        ", the computer chose " + computerChoice +
+        ".<br>" + result;
 
-
-
-
+    // If last round just finished, show final list of guesses
+    if (round === roundTotal) {
+        document.getElementById("result").innerHTML +=
+            "<br><br>Game Over!<br>Your guesses were: " + userGuesses.join(", ");
+    }
 }
+
